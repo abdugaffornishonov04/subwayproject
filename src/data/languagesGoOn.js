21d0e413a05040b3f79979e8e32c4e8bf476f457
Devgaffor <abdugaffor0042@gmail.com>
2023-12-20 22:22:20 +0500

    some changes

---
+++ src/data/languagesGoOn.js
@@ -0,0 +1,14 @@
+export const LANGGOON = [
+  {
+    id: 1,
+    text: "Next"
+  },
+  {
+    id: 2,
+    text: "Далее"
+  },
+  {
+    id: 3,
+    text: "Davom etish"
+  },
+]
--- src/pages/LanguagePage.jsx
+++ src/pages/LanguagePage.jsx
@@ -1,6 +1,20 @@
+import { useState } from "react";
+import { LANGGOON } from "../data/languagesGoOn";
 import { LANGUAGES } from "../data/languagesdata";
 
 const LanguagePage = () => {
+  const [selectedButtonId, setSelectedButtonId] = useState(null);
+  const [langNextButton, setLangNextButton] = useState(false);
+
+  const nextButtonsIdObject = LANGGOON.find((el) => el.id === selectedButtonId);
+
+  const nextButtonsText = nextButtonsIdObject ? nextButtonsIdObject.text : "";
+
+  const langButtonHandler = (id) => {
+    setSelectedButtonId(id);
+    setLangNextButton(true);
+  };
+
   return (
     <section className="lang-page">
       <div className="lang-page-shade">
@@ -8,13 +22,29 @@ const LanguagePage = () => {
           <h1 className="lang-page-title">Tilni Tanlang</h1>
           <div className="lang-page-btns">
             {LANGUAGES.map((el, i) => (
-              <button key={i} className="lang-page-button">
+              <button
+                onClick={() => langButtonHandler(el.id)}
+                key={i}
+                className={
+                  selectedButtonId === el.id
+                    ? "lang-page-button-selected"
+                    : "lang-page-button"
+                }
+              >
                 <img src={el.image} alt={el.lang} />
                 <p>{el.lang}</p>
               </button>
             ))}
           </div>
         </div>
+        <button
+          className={
+            langNextButton ? "lang-go-next-page-show" : "lang-go-next-page"
+          }
+        >
+          <p>{nextButtonsText} </p>
+          <img src="/arrow-right.png" alt="" />
+        </button>
       </div>
     </section>
   );
